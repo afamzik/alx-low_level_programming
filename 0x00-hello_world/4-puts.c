474aa6dd42f005718d4fa0f2179bb696d794f311
Ezike Afamefuna <afamefunameanwellezike@gmail.com>
2022-09-10 20:12:18 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+void main(void)
 {
 	char xy[] = "Programming is like building a multilingual puzzle";
+
 	puts(xy);
-	return 0;
+	return (0);
 }

b2e80fb7581234772385238ecd4c91101aa3d74e
Ezike Afamefuna <afamefunameanwellezike@gmail.com>
2022-09-10 20:53:36 -0700

    fd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 /**
  * main - this is the main function
+ * void: its empty
  */
-int main(void)
+void main(void)
 {
+/*
+ *char xy[] - the argument
+ */
 	char xy[] = "\"Programming is like building a multilingual puzzle";
 
+/**
+ * puts - outputs strings
+ */
 	puts(xy);
+/**
+ * return - returns 0
+ */
 	return (0);
 }

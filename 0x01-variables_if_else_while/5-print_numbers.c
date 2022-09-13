e5fb5f26ea45d64c464bb1c7ca84c7fb62c859fe
Ezike Afamefuna <afamefunameanwellezike@gmail.com>
2022-09-14 00:53:07 +0100

    dsd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - the main ffunction
+ *Return: return 0
+ */
+
+int main(void)
+{
+
+	for (int x = 0; x < 10; x++)
+	{
+		printf("%i", x);
+	}
+
+	printf("\n");
+
+	return (0);
+}

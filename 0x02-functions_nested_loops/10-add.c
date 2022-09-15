172e8ca9bc683c1a7181877da900e53c3428a293
Ezike Afamefuna <afamefunameanwellezike@gmail.com>
2022-09-15 06:19:55 +0100

    f

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+ #include "main.h"
+
+/**
+ *add - add funtion
+ *@x: the argument
+ *@y: another argument
+ *Return: returns the required value
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}

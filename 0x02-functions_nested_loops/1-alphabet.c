20f93827e92d2e14701f08d9d7b9e061b6532aa9
Ezike Afamefuna <afamefunameanwellezike@gmail.com>
2022-09-15 02:15:36 +0100

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet - this function prints alphabets
+ *
+ *Returns: 0 as usual
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(xc);
+		x++;
+	}
+
+	_putchar('\n');
+}

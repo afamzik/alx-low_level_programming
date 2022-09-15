f1453da995325228045b8894318f57e0a3ec82f0
Ezike Afamefuna <afamefunameanwellezike@gmail.com>
2022-09-15 03:22:28 +0100

    ds

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_sign - fuction that tells  you the sign of a number
+ *@n: the is the argument
+ *Return: returns a value
+ */
+
+int print_sign(int n)
+{
+	if (n  > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

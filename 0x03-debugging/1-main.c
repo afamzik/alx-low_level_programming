a95adb6169f0db7d4705d270c5b951a48339f669
Ezike Afamefuna <afamefunameanwellezike@gmail.com>
2022-09-16 00:22:07 +0100

    de

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - cause continuous looping
+ *Return: 0 is the value to be returned
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	*while (i < 10)
+	*{
+	*putchar(1);
+	*}
+	*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}

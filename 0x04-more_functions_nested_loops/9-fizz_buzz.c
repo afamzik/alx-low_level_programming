3f5f92805be4f152494d15cb1af7a1ecd487770f
Ezike <afamefunameanwellezike@gmail.com>
2022-09-16 22:39:29 -0400

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,25 @@
 
 int main(void)
 {
-	int num = 1
+	int x;
 
-	while (num++ < 100)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((num % 3) == 0)
+		else if ((x % 3) == 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((num % 5) == 0)
+		else if ((x % 5) == 0)
 		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-		 	}
-			else
-			{
-				printf("Buzz");
-			}
+			printf("Buzz ");
 		}
-	
+		else
+		{
+			printf("%i ", x);
+		}
+	}
+}

5a62b8f9ea217cf7fa612fd95fd6127b24570ed9
Ezike <afamefunameanwellezike@gmail.com>
2022-09-22 02:24:46 -0400

    de

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	while (s[i] != '\0' && s[i] != c)
-	{
+	while (*(s + i) != '\0' && *(s + i) != c)
+	i++;
 
-	if (s[i] == c)
-		return (&s[i]);
+	if (*(s + i) == c)
+		return (s + i);
 	else
 		return (NULL);
-	i++;
-	}
 
 }

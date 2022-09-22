5b40b7beb208c1d2cdcf2adc42bf518b06fd31e3
Ezike <afamefunameanwellezike@gmail.com>
2022-09-22 02:10:28 -0400

    match

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#define NULL 0
+/**
+*_strchr - this matches orlocates a character in a string
+*@s: the string of characters
+*@c: the character to be returned
+*Return: the pointer returns first occurrence
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
+}

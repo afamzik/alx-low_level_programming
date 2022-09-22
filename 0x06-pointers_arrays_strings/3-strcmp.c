ee7849e25c8f13725ea18d7a4a46eb8e0b58f562
Ezike <afamefunameanwellezike@gmail.com>
2022-09-21 22:58:44 -0400

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+*_strcmp - the fuction that compares
+*@s1: the first string
+*@s2: the second string
+*Return: returns value
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, diff = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+	}
+
+	return (diff);
+}

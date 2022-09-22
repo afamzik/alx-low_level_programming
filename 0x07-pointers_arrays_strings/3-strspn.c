bc47d011f9e7e05a009a953dc6424e6bc62111e5
Ezike <afamefunameanwellezike@gmail.com>
2022-09-22 04:58:47 -0400

    span

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+*_strspn - this function spells out the lenght of
+* the initial matching substring of a string
+*@s: the string to  be searched
+*@accept: the searched for
+*Return: returns the span of the string matched
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	int matches = 0;
+
+	while (s[i] != '\0')	/* loops through the string s */
+	{
+		for (j = 0; accept[j] != '\0'; j++) /*loops the string searched for*/
+		{
+			if (s[i] == accept[j]) /*note and first the first match*/
+			{
+				matches++;
+				break;
+			}
+
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+				return (matches); /*returns matches if idx do not match*/
+		}
+
+		i++;
+	}
+
+	return (matches);
+}

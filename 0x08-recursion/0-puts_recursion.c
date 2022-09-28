249960fb411035e35b2f18216574339cf46f1b0b
Ezike <afamefunameanwellezike@gmail.com>
2022-09-27 21:56:13 -0400

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion - this function prints a string
+*@s: string that will be printed
+*Return:  returns nothing
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory contains recursive c files

dd1376f60ebde2f5aad0f131fbf80be5097b1ab4
Ezike <afamefunameanwellezike@gmail.com>
2022-10-02 18:12:54 +0100

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - the main function
+*@x: the number of args
+*@mult: the number to multiply
+*Return: returns 0 on success and 1 otherwise
+*/
+
+int main(int x, char *mult[])
+{
+	if (x != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	printf("%d\n", atoi(mult[1]) * atoi(mult[2]));
+	return (0);
+}

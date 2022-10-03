eb53687bc0410fe73d8080897b587361d594540e
Ezike <afamefunameanwellezike@gmail.com>
2022-10-03 12:16:04 +0100

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+/* this  will define the size _t */
+
+/**
+* array_iterator - this program runs a function on an array
+*@array: array used
+*@size: the array size
+*@action: the pointer to the fuction
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	/*this is to match the data type*/
+	size_t x;
+
+	if (array != NULL && action != NULL)
+		for (x = 0; x < size; x++)
+			action(*(array + 1));
+}

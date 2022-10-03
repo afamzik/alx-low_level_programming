0b34205aece1dde2a00a20e6a78ed32b41156903
Ezike <afamefunameanwellezike@gmail.com>
2022-10-03 02:01:57 +0100

    grace

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,21 +22,26 @@ char *str_concat(char *s1, char *s2)
 	/*size of first string */
 	for (x  = 0; s1[x]; x++)
 		size1++;
+	/*size of second string */
 	for (y = 0;  s2[y]; y++)
 		size2++;
-
+	/*size to be allocated */
 	size = size1 + size2;
-
+	/*size incremented for null terminator */
 	size++;
 
 	con_cat = malloc(sizeof(char) * size);
 
 	if (con_cat == NULL)
 		return (NULL);
-
+	/*copies s1 to con_cat*/
 	x = 0;
 	for (x = 0; x < size1; x++)
 		con_cat[x] = s1[x];
+	/**
+	*copies s2 to con_cat starting from where s1 STOPPED
+	* IN x++(= size1) as it loops through s2
+	*/
 	y = 0;
 	for (y = 0; y < size2; y++)
 		con_cat[x] = s2[y], x++;

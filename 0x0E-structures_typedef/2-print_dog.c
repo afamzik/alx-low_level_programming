562fbfde455df3b6606d693f72195e15d95df480
Ezike <afamefunameanwellezike@gmail.com>
2022-10-03 11:10:41 +0100

    ds

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,12 +12,23 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	(d->name == NULL) ? printf("name: (nil)\n")
-		: printf("name: %s\n", d->name);
+	if (d->name == NULL)
+	{
+		printf("name: (nil)\n");
+	}
+	else
+		printf("name: %s\n", d->name);
 
-	printf("Age: %f\n", d->age);
+	if (d->age == '\0')
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 
-	(d->owner == NULL) ? printf("owner: (nil)\n")
-		: printf("owner: %s\n", d->owner);
+	if (d->owner == NULL)
+	{
+		printf("owner: (nil)\n");
+	}
+	else
+		printf("owner: %s\n", d->owner);
 
 }

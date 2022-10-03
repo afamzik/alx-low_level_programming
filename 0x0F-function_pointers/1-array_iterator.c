15fd5a7fc783c98c0cacb7daa36c5947f1421037
Ezike <afamefunameanwellezike@gmail.com>
2022-10-03 12:32:55 +0100

    ssde

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,5 +16,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL && action != NULL)
 		for (x = 0; x < size; x++)
-			action(*(array + 1));
+			action(*(array + x));
 }

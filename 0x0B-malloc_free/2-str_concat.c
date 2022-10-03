1f838d6973a2b43a6930e7f3c36008ca73e3bff0
Ezike <afamefunameanwellezike@gmail.com>
2022-10-03 01:29:55 +0100

    asde

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *con_cat;
-	int size1, size2, size;
+	int size1 = 0, size2 = 0,  size = 0;
 	int x;
 
 	if (s1 == NULL)
@@ -20,28 +20,24 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 	/*size of first string */
-	for (x = 0; s1[x]; x++)
+	for (x  = 0; s1[x]; x++)
 		size1++;
-	/*size of second string*/
-	for (x = 0; s2[x]; x++)
+	for (x = 0;  s2[x]; x++)
 		size2++;
-	/*total size of two strings*/
-	size =  size1 + size2;
-	/* null allocation of memory*/
+
+	size = size1 + size2;
+
 	size++;
 
 	con_cat = malloc(sizeof(char) * size);
 
 	if (con_cat == NULL)
 		return (NULL);
-
-	/* copy s1 to cocat */
 	x = 0;
 	for (x = 0; x < size1; x++)
-		*(con_cat + x) = *(s1 + x);
-	/* copy s2 to concat */
-	x = 0;
-	for (x = size1; x < size; x++)
+		con_cat[x] = s1[x];
+	x = size1;
+	for (x = size1; x < size2; x++)
 		con_cat[x] = s2[x];
 
 	return (con_cat);

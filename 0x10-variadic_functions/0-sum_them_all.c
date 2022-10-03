4934cee0c657909f8b55dfa0eae2d8b560341149
Ezike <afamefunameanwellezike@gmail.com>
2022-10-03 16:18:31 +0100

    dse

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,9 +17,12 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
+	/*list valdation*/
 	va_start(valist, n);
+
+	/*loops throug list*/
 	for (x = 0; x < n; x++)
-		sum = sum + va_arg(valist, int);
+		sum += va_arg(valist, int);
 	va_end(valist);
 	return (sum);
 }

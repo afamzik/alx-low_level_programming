fef3951c355ae061cae9dfee3d43ca70c0e19a17
Ezike <afamefunameanwellezike@gmail.com>
2022-10-03 17:44:50 +0100

    sdeweeew

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,14 +14,14 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int x;
 	int sum = 0;
 
-	if (n == 0)
+	if (n =< 0)
 		return (0);
 
 	/*list valdation*/
 	va_start(valist, n);
 
 	/*loops throug list*/
-	for (x = 1; x < n; x++)
+	for (x = 0; x < n; x++)
 		sum += va_arg(valist, int);
 	va_end(valist);
 	return (sum);
